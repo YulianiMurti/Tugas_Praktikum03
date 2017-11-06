8b2d71245e15ddf1edd3356de21cbfae8dd0a223
YulianiMurti <32427693+YulianiMurti@users.noreply.github.com>
2017-11-06 14:36:18 +0700

    Jenis Bilangan

--- Menentukan_Jenis_Bilangan.cpp
+++ Menentukan_Jenis_Bilangan.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main(){
+	int a;
+	cout<<"MENENTUKAN SEBUAH PROGRAM JENIS BILANGAN"<<endl;
+	cout<<""<<endl;
+	cout<<"MASUKAN SEBUAH BILANGAN : ";cin>>a;
+	cout<<""<<endl;
+	if (a < 0)
+	{
+		cout<<"Yang anda input adalah sebuah bilangan negatif"<<endl;
+	}
+	
+	else if (a>0)
+	{
+		cout<<"Yang anda input adalah sebuah bilangan positif"<<endl;
+	}
+	else if (a==0)
+	
+		cout<<"Yang anda input adalah sebuah bilangan nol "<<endl;
 
+	return 0;
 }
