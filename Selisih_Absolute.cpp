ae538782ebff74d80a4d045a2343e0dd45969be4
YulianiMurti <32427693+YulianiMurti@users.noreply.github.com>
2017-11-06 14:32:10 +0700

    mencari selisih

--- Selisih_Absolute.cpp
+++ Selisih_Absolute.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 int main(){
+	int a,b,selisih;
+	cout<<" Input Bilangan a : "; cin>>a;
+	cout<<" Input Bilangan b : "; cin>>b;
 
+if (a>b){
+	selisih=a-b;
+	
+	cout<<"Selisih Nilai A dan B yaitu "<<selisih;
+}
+
+else if (b>a){
+	selisih=b-a;
+
+	cout<<"Selisih Nilai B dan A yaitu "<<selisih;
+}
+	return 0;
 }
